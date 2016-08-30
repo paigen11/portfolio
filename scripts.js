e46595f59e629a700d8a211454a15e89d63b18a2
paigen11 <paigen11@gmail.com>
2016-08-30 09:28:30 -0400

    Add scroll effect to portfolio

--- scripts.js
+++ scripts.js
@@ -1,5 +1,21 @@
-
 // On document ready:
+$(document).ready(function(){
+
+	$('body').scrollspy({target: '.navbar', offset: 50});
+
+	$('.myNavBar a').on('click', function(event){
+		if(this.hash !== ''){
+			event.preventDefault();
+			var hash = this.hash;
+
+			$('html, body').animate({
+				scrollTop: $(hash).offset().top
+			}, 800, function(){
+				window.location.hash = hash;
+			});
+		}
+	});
+});
 
 $(function(){
 
