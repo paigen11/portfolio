c4525b1a15ab1c3f717b3f5b6474ebc20d1425ef
paigen11 <paigen11@gmail.com>
2016-09-30 16:23:48 -0400

    Added animations and overlay for projects

--- scripts.js
+++ scripts.js
@@ -20,10 +20,35 @@ $(document).ready(function(){
 		}
 	});
 
-	// $('.contact-form').submit(function(){
-	// 	$('.thank-you').text("Thanks for your note! I'll be in touch soon.");
-	// })
+	var animationDelay = 5000;
+
+	animateHeadline($('.headline'));
+
+	function animateHeadline($headlines){
+		$headlines.each(function(){
+			var headline = $(this);
+			//trigger animation
+			setTimeout(function(){ hideWord( headline.find('.is-visible'))}, animationDelay);
+		});
+	}
+
+	function hideWord($word) {
+		var nextWord = takeNext($word);
+		switchWord($word, nextWord);
+		setTimeout(function(){ hideWord(nextWord) }, animationDelay);
+	}
+	 
+	function takeNext($word) {
+		return (!$word.is(':last-child')) ? $word.next() : $word.parent().children().eq(0);
+	}
+	 
+	function switchWord($oldWord, $newWord) {
+		$oldWord.removeClass('is-visible').addClass('is-hidden');
+		$newWord.removeClass('is-hidden').addClass('is-visible');
+	}
+
 });
+
 
 $(function(){
 
